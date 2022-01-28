c8e3c1fb56a8b45c549f9470507693151be34553
In5lux <nikos_almeryda@mail.ru>
2022-01-28 20:19:21 +0300

    Output data

--- services/api.service.js
+++ services/api.service.js
@@ -1,6 +1,28 @@
-//import https from 'https';
 import axios from 'axios';
-import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js'
+import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
+
+const getIcon = (icon) => {
+	switch (icon.slice(0, -1)) {
+		case '01':
+			return '☀️';
+		case '02':
+			return '🌤️';
+		case '03':
+			return '☁️';
+		case '04':
+			return '☁️';
+		case '09':
+			return '🌧️';
+		case '10':
+			return '🌦️';
+		case '11':
+			return '🌩️';
+		case '13':
+			return '❄️';
+		case '50':
+			return '🌫️';
+	}
+};
 
 const getWeather = async (city) => {
 	const token = await getKeyValue(TOKEN_DICTIONARY.token);
@@ -15,22 +37,7 @@ const getWeather = async (city) => {
 			units: 'metric'
 		}
 	});
-	console.log(data);
 	return data;
-	// const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-	// url.searchParams.append('q', city);
-	// url.searchParams.append('appid', token);
-	// url.searchParams.append('lang', 'ru');
-	// url.searchParams.append('units', 'metric');
-	// https.get(url, (response) => {
-	// 	let res = '';
-	// 	response.on('data', (chunk) => {
-	// 		res += chunk;
-	// 	});
-	// 	response.on('end', () => {
-	// 		console.log(res)
-	// 	});
-	// });
 };
 
-export { getWeather };
+export { getWeather, getIcon };
--- weather.js
+++ weather.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
 import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
-import { getWeather } from './services/api.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { getKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -18,13 +19,31 @@ const saveToken = async (token) => {
 	}
 };
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранен');
+
+	} catch (e) {
+		printError(e.message);
+	}
+};
+
 const getForcast = async () => {
 	try {
-		const weather = await getWeather('moscow');
-		console.log(weather);
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		const icon = getIcon(weather.weather[0].icon);
+		printWeather(weather, icon);
 	} catch (e) {
 		if (e?.response?.status == 404) {
 			printError('Неверно указан город');
+		} else if (e?.response?.status == 400) {
+			printError('Не указан город');
 		} else if (e?.response?.status == 401) {
 			printError('Неверно указан токен');
 		} else {
@@ -37,10 +56,10 @@ const getForcast = async () => {
 const initCLI = () => {
 	const args = getArgs(process.argv)
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 	if (args.s) {
-		console.log('Сохранить город');
+		return saveCity(args.s);
 	}
 	if (args.t) {
 		return saveToken(args.t);
